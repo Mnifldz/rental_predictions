098cf67328c18323de8498c18a6f96f093307b34
Mnifldz <paulalexanderdavid@gmail.com>
2025-12-08 00:46:05 +0000

    File to centralize feature sets.

---
+++ features.py
@@ -0,0 +1,19 @@
+categorical_cols = [
+    "District",
+    "Neighborhood",
+    "PropertyType",
+    "CancellationPolicy",
+    "RoomType"
+]
+
+numeric_cols = [
+    "Accommodates",
+    "Bathrooms_rounded",
+    "Bedrooms_rounded",
+    "CleaningFee",
+    "Latitude",
+    "Longitude",
+    "ReviewRating"
+]
+
+target_col = "Price"
